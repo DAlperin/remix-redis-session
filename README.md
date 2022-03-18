e8a8181ebe951c3e4e555da118a87787292bcab4
Dov Alperin <git@dov.dev>
2022-03-18 14:06:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# remix-redis-session
+
+A simple library to use redis as your session backing store in your remix project
+
+## Api
+
+```ts
+createRedisSessionStorage({
+    cookie: SessionIdStorageStrategy["cookie"]
+    options: {
+        redisConfig?: Redis.RedisOptions,
+        redisClient?: Redis.Redis
+    }
+})
+```
+
+## Examples
+
+```ts
+// With redisConfig
+export const sessionStorage = createRedisSessionStorage({
+    cookie: {
+        name: "example",
+        secure: true,
+        sameSite: "lax",
+        secrets: ["s3cr3t"],
+        path: "/",
+        httpOnly: true,
+    },
+    options: {
+        redisConfig: {
+            port: 6379,
+            family: 6,
+            host: process.env.REDIS_HOST,
+            password: process.env.REDIS_PASSWORD ?? undefined,
+        }
+    }
+});
+
+// With redisClient
+const redis = new Redis({
+    port: 6379,
+    family: 6,
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD ?? undefined,
+});
+
+export const sessionStorage = createRedisSessionStorage({
+    cookie: {
+        name: "example",
+        secure: true,
+        sameSite: "lax",
+        secrets: ["s3cr3t"],
+        path: "/",
+        httpOnly: true,
+    },
+    options: {
+        redisClient: redis
+    }
+});
+```
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import { SessionIdStorageStrategy, SessionStorage, createSessionStorage } from "@remix-run/server-runtime/sessions";
+import { randomBytes as crypto_randomBytes } from "crypto";
+import Redis from "ioredis";
+
+function genRandomID(): string {
+    const randomBytes = crypto_randomBytes(8);
+    return Buffer.from(randomBytes).toString("hex");
+}
+
+const expiresToSeconds = (expires: Date) => {
+    const now = new Date();
+    const expiresDate = new Date(expires);
+    const secondsDelta = expiresDate.getSeconds() - now.getSeconds();
+    return secondsDelta < 0 ? 0 : secondsDelta;
+};
+
+type redisSessionArguments = {
+    cookie: SessionIdStorageStrategy["cookie"];
+    options: {
+        redisConfig?: Redis.RedisOptions,
+        redisClient?: Redis.Redis
+    }
+};
+
+export function createRedisSessionStorage({
+    cookie,
+    options
+}: redisSessionArguments): SessionStorage {
+    let redis: Redis.Redis
+    if (options.redisClient) {
+        redis = options.redisClient
+    } else if (options.redisConfig) {
+        redis = new Redis(options.redisConfig);
+    } else {
+        throw new Error("Need to provide either options.RedisConfig or options.redisClient")
+    }
+
+    return createSessionStorage({
+        cookie,
+        async createData(data, expires) {
+            const id = genRandomID();
+            if (expires) {
+                await redis.set(
+                    id,
+                    JSON.stringify(data),
+                    "EX",
+                    expiresToSeconds(expires)
+                );
+            } else {
+                await redis.set(id, JSON.stringify(data));
+            }
+            return id;
+        },
+        async readData(id) {
+            const data = await redis.get(id);
+            if (data) {
+                return JSON.parse(data);
+            }
+            return null;
+        },
+        async updateData(id, data, expires) {
+            if (expires) {
+                await redis.set(
+                    id,
+                    JSON.stringify(data),
+                    "EX",
+                    expiresToSeconds(expires)
+                );
+            } else {
+                await redis.set(id, JSON.stringify(data));
+            }
+        },
+        async deleteData(id) {
+            await redis.del(id);
+        },
+    });
+}
